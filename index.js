35be1053f7f8c03e8cf9ec22a0b8b6fbbe609c76
ThamizhAnban <thamizhanban2006@gmail.com>
2025-02-13 10:07:11 +0530

    Database created

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
-const express = require('express');
-const { resolve } = require('path');
-
-const app = express();
+const express = require("express")
+const app  = express()
+require('dotenv').config();
+const uri = process.env.uri;
+const mongoose = require("mongoose");
 const port = 3010;
-
 app.use(express.static('static'));
 
 app.get('/', (req, res) => {
   res.sendFile(resolve(__dirname, 'pages/index.html'));
 });
 
+
+mongoose.connect(uri)
+.then(()=>{
+    console.log(` Connected to database`)
+})
+.catch((err)=>{
+    console.log("Error:",err);
+})
+
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-});
+    console.log(`Example app listening at http://localhost:${port}`);
+  });
